ecb79dc3578f35e14c4d2e2433399735be0e8396
dhgudtjr0306 <dhgudtjr0306@gmail.com>
2019-08-11 00:02:11 +0900

    Merge(hsoh0306)

---
+++ InSeoul/app/src/main/java/com/inseoul/search/SearchActivity.kt
@@ -0,0 +1,21 @@
+package com.inseoul.search
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.inseoul.R
+import com.inseoul.make_plan.MakePlanActivity
+import kotlinx.android.synthetic.main.activity_search.*
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        initBtn()
+    }
+
+    fun initBtn(){
+
+    }
+}
