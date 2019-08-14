623284a5582a12386d2c8e77032db1a86dc4a981
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2019-08-14 23:01:29 +0900

    toolbar 수정
    toolbar 기본 수정

--- InSeoul/app/src/main/java/com/inseoul/search/SearchActivity.kt
+++ InSeoul/app/src/main/java/com/inseoul/search/SearchActivity.kt
@@ -1,9 +1,12 @@
 package com.inseoul.search
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,6 +20,21 @@ class SearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search)
         initTest()
         initBtn()
+
+
+        //toolbar 코드
+        //toolbar 커스텀 코드
+        val mtoolbar = findViewById(R.id.toolbar_search) as Toolbar
+        setSupportActionBar(mtoolbar)
+        // Get the ActionBar here to configure the way it behaves.
+        val actionBar = supportActionBar
+        actionBar!!.setDisplayShowCustomEnabled(true) //커스터마이징 하기 위해 필요
+        actionBar.setDisplayShowTitleEnabled(false)
+
+        actionBar.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp) //뒤로가기 버튼을 본인이 만든 아이콘으로 하기 위해 필요
+        mtoolbar.title = "검색"
+        mtoolbar.setTitleTextColor(Color.WHITE)
     }
 
     fun initBtn(){
@@ -56,5 +74,18 @@ class SearchActivity : AppCompatActivity() {
         adapter = SearchAdapter(this, listener, test)
         recyclerView.adapter = adapter
         recyclerView.addItemDecoration(DividerItemDecoration(this, 1))
+    }
+
+    
+
+    //toolbar에서 back 버튼
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
