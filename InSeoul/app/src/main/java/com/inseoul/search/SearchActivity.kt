9340fbbb5a0cd3c4bd81468df49662f8b6954e93
dhgudtjr0306 <dhgudtjr0306@gmail.com>
2019-08-19 01:29:12 +0900

    hsoh0306

--- InSeoul/app/src/main/java/com/inseoul/search/SearchActivity.kt
+++ InSeoul/app/src/main/java/com/inseoul/search/SearchActivity.kt
@@ -33,7 +33,7 @@ class SearchActivity : AppCompatActivity() {
         actionBar.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
         actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp) //뒤로가기 버튼을 본인이 만든 아이콘으로 하기 위해 필요
         mtoolbar.title = "검색"
-        mtoolbar.setTitleTextColor(Color.WHITE)
+//        mtoolbar.setTitleTextColor("#000000")
     }
 
     fun initBtn(){
