8479192f3e0b7c6fa6b957eea71c8d6e3728d586
KwonSunJae <ksun4131@gmail.com>
2019-08-23 01:30:45 +0900

    SERVER

--- New/src/Server/Inseoul_map_data.java
+++ New/src/Server/Inseoul_map_data.java
@@ -21,6 +21,7 @@ public class Inseoul_map_data {
         Connection con = null;
         PreparedStatement st = null;
 		//run("C:\\Users\\DGCOM\\Desktop\\Seoul_hot\\Seoul_Hot\\data.csv","euc-kr");
+        
 		try
 		{
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -28,21 +29,37 @@ public class Inseoul_map_data {
 			Statement stmt = conn.createStatement();
 			br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\DGCOM\\Desktop\\Seoul_hot\\Seoul_Hot\\data.csv"),"euc-kr"));
             while ((line = br.readLine()) != null) {
+            	if(line.equals(",,,,,,"))continue;
             	String[] field = line.split(cvsSplitBy);
-            	 if(field[1].equals("고유번호"))continue;
             	
-                String sql = "Insert into InSeoul_map values(?,?,?,?,?) on duplicat key update Spot_new ="+field[3].split("서울 ")[2].split(",")[0];
+            	 if(field[1].equals("고유번호"))continue;
+        
+                String sql = "Insert into InSeoul_map values(?,?,?,?,?)";
     			st = conn.prepareStatement(sql);
     			st.setInt(1, Integer.parseInt(field[1]));
-    			st.setString(2,  field[3].split("서울 ")[1].split(" ")[0]);
+    			st.setString(2, field[3].split("서울")[1].split(" ")[1]);
     			
-    			st.setString(3, field[3].split("서울 ")[1].split(" ")[1]);
-    			st.setString(3, field[3].split("서울 ")[1].split(" ")[2]);
-    			st.setString(3, field[3].split("서울 ")[2].split(",")[0]);
+    			st.setString(3, field[3].split("서울")[1].split(" ")[2]);
+    			st.setString(4, field[3].split("서울")[1].split(" ")[3]);
+    			int i =4;
+    			if(field[4].equals(""))continue;
+    			while(!field[i].contains("서울")) {
+    				i+=1;
+    			}
+    			
+    			if( field[i].split("서울")[1].split(",")[0].contains("(")) {
+    				st.setString(5, field[i].split("서울")[1].split(",")[0].split("(")[0]);
+        			
+        			System.out.println(st);
+        			
+        			st.executeUpdate();
+        			continue;
+    			}
+    			st.setString(5, field[i].split("서울")[1].split(",")[0]);
     			
     			System.out.println(st);
     			
-    			//st.executeUpdate();
+    			st.executeUpdate();
             }
             conn.close();
 		}catch( ClassNotFoundException e )
@@ -56,37 +73,6 @@ public class Inseoul_map_data {
 		{
 			System.out.println("기타 오류 "+e);
 		}
-	}
-	
-	private static void run(String path, String encoding) {
-        BufferedReader br = null;
-        String line;
-        String cvsSplitBy = ",";
-
-        try {
-            br = new BufferedReader(new InputStreamReader(new FileInputStream(path), encoding));
-            while ((line = br.readLine()) != null) {
-                String[] field = line.split(cvsSplitBy);
-                for(int i =0;i <field.length ;i++) {
-                	System.out.print(field[i] );
-                }
-                System.out.print("\n");
-                
-                
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 	
 	
