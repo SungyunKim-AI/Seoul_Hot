96660ba06ceb83a4e805f5fc005cf34cd4cfcb95
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2019-09-02 02:08:35 +0900

    searchDetail

--- InSeoul/app/src/main/java/com/inseoul/search/SearchDetail.java
+++ InSeoul/app/src/main/java/com/inseoul/search/SearchDetail.java
@@ -1,8 +1,11 @@
 package com.inseoul.search;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,21 +17,34 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.inseoul.R;
 import com.inseoul.add_place.AddPlaceActivity;
+import com.inseoul.manage_member.SaveSharedPreference;
+import com.inseoul.manage_schedules.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class SearchDetail extends AppCompatActivity implements View.OnClickListener {
 
     String m_name = null;
-    private int plan_count=3;
+    private int plan_count;
     private Button btnAlert;
 
 
     /////////////TEST CODE///////////////
     ArrayList<planlist> testlist = new ArrayList<>();
     private int upsoID;
+    private ArrayList<Integer> planidarray = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +58,43 @@ public class SearchDetail extends AppCompatActivity implements View.OnClickListe
     }
 
     public void init(){
-
+        SharedPreferences sf= getSharedPreferences("InSeoul",0);
+        String idNUM = sf.getString("UserID","===================");
         SearchItem searchItem = getIntent().getParcelableExtra("placeData");
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try
+                {
+                    Log.d("dd", response);
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONArray success= jsonResponse.getJSONArray("response");
+                    int count=0;
+                    plan_count = success.length();
+                    while (count<success.length()){
+                        JSONObject object = success.getJSONObject(count);
+                        planidarray.add(object.getInt("PLANID"));
+                        Log.d(this.getClass().getName(), planidarray.toString());
+
+
+                        count++;
+
+                    }
+                    ShowPlanTask showPlanTask = new ShowPlanTask();
+                    showPlanTask.execute();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        idnumRequest idnumrequest = new idnumRequest(idNUM, responseListener);
+        RequestQueue queue = Volley.newRequestQueue(SearchDetail.this);
+        queue.add(idnumrequest);
+
 
         /////////////TEST CODE///////////////
-        for (int i=0; i<plan_count;i++){
-            testlist.add(new planlist("testTitle"+i, "testDate"+i));
-        }
+
 
 
         btnAlert = (Button)findViewById(R.id.add_my_list);
@@ -84,7 +130,95 @@ public class SearchDetail extends AppCompatActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
+    public class ShowPlanTask extends AsyncTask<Void,Void,String> {
 
+        String target;
+        int PlanID;
+        ProgressDialog asyncDialog = new ProgressDialog(SearchDetail.this);
+
+
+
+
+        @Override
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+
+            target="http://ksun1234.cafe24.com/ShowPlan.php"; // 웹 호스팅 정보를 받기위한  php 파일 주소
+
+
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                Log.d("asyncTask","Strart");
+                URL url = new URL(target);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                Log.d("asyncTask","Connect");
+                InputStream inputStream = con.getInputStream();
+                StringBuilder stringBuilder = new StringBuilder();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String json;
+                while ((json = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(json + "\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                con.disconnect();
+                return stringBuilder.toString().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void onPostExecute(String result){
+
+
+            try{
+                Log.d("asyncTask","onPostExecute");
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray jsonArray = jsonObject.getJSONArray("response");
+
+                int count=0;
+                while (count<jsonArray.length()){
+                    Log.d("asyncTask","Reading"+jsonArray);
+                    JSONObject object = jsonArray.getJSONObject(count);
+                    int planID = object.getInt("H");
+                    Log.d("as",planidarray.toString());
+                    if(planidarray.contains(planID)){
+                        Log.d("async","ss"+ Integer.toString(planID));
+
+                        testlist.add(new planlist(object.getString("TripName"), object.getString("DPDATE"), object.getInt("H")));
+
+
+
+                    }
+
+
+
+
+
+
+                    count++;
+
+                }
+
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            asyncDialog.dismiss();
+        }
+        protected void onCancelled()
+        {
+
+            super.onCancelled();
+        }
+    }
     //다이얼 로그 세팅 onClick
     @Override
     public void onClick(View v) {
@@ -161,6 +295,15 @@ public class SearchDetail extends AppCompatActivity implements View.OnClickListe
         String title;
         String date;
 
+        public int getPlanid() {
+            return planid;
+        }
+
+        public void setPlanid(int planid) {
+            this.planid = planid;
+        }
+
+        int planid;
         public String getTitle() {
             return title;
         }
@@ -177,9 +320,10 @@ public class SearchDetail extends AppCompatActivity implements View.OnClickListe
             this.date = date;
         }
 
-        public planlist(String title, String date){
+        public planlist(String title, String date, int planid){
             this.title = title;
             this.date = date;
+            this.planid = planid;
         }
     }
 }
