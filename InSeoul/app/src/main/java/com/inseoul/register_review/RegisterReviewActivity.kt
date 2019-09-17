1493d17e4a7d09bbb30e6ea9e09da5a24609e986
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2019-09-18 00:46:14 +0900

    2

--- InSeoul/app/src/main/java/com/inseoul/register_review/RegisterReviewActivity.kt
+++ InSeoul/app/src/main/java/com/inseoul/register_review/RegisterReviewActivity.kt
@@ -353,4 +353,74 @@ class RegisterReviewActivity : AppCompatActivity()  {
         }
         return super.onOptionsItemSelected(item)
     }
+    public void HTTpfileUpload(){
+        String pathToOurFile = imageFilePath;
+        String urlServer = "http://ksun1234.cafe24.com/UploadIMG.php";
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        try {
+            FileInputStream mFileInputStream = new FileInputStream(imageFilePath);
+            URL connectUrl = new URL(urlServer);
+            Log.d("Test", "mFileInputStream  is " + imageFileName);
+            Log.d("Test", "mFileInputStream  is " + imageFilePath);
+            // open connection
+            HttpURLConnection conn = (HttpURLConnection)connectUrl.openConnection();
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+            // write data
+            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + imageFileName +"\"" + lineEnd);
+            dos.writeBytes(lineEnd);
+
+            int bytesAvailable = mFileInputStream.available();
+            int maxBufferSize = 1024;
+            int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+
+            byte[] buffer = new byte[bufferSize];
+            int bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
+
+            Log.d("Test", "image byte is " + bytesRead);
+
+            // read image
+            while (bytesRead > 0) {
+                dos.write(buffer, 0, bufferSize);
+                bytesAvailable = mFileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
+            }
+
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            // close streams
+            Log.e("Test" , "File is written");
+            mFileInputStream.close();
+            dos.flush(); // finish upload...
+
+            // get response
+            int ch;
+            InputStream is = conn.getInputStream();
+            StringBuffer b =new StringBuffer();
+            while( ( ch = is.read() ) != -1 ){
+                b.append( (char)ch );
+            }
+            String s=b.toString();
+            Log.e("Test", "result = " + s);
+
+            dos.close();
+            conn.disconnect();
+
+
+        } catch (Exception e) {
+            Log.d("Test", "exception " + e.getMessage());
+            // TODO: handle exception
+        }
+    }
 }
