954498ca96afe203a6b91545e6f20d007abc225a
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2019-09-27 03:49:05 +0900

    CheckLikeRequest

---
+++ InSeoul/app/src/main/java/com/inseoul/Server/CheckLikeRequest.java
@@ -0,0 +1,31 @@
+package com.inseoul.Server;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CheckLikeRequest extends StringRequest {
+    final static private String URL= "http://ksun1234.cafe24.com/Dislik.php";
+    private Map<String, String> parameters;
+
+    public CheckLikeRequest(String userID, String id,  Response.Listener<String> listener){
+
+        super(Method.POST, URL, listener, null);
+        parameters=new HashMap<>();
+        parameters.put("ReviewID", userID);
+        parameters.put("UserID", id);
+
+
+
+
+
+    }
+    @Override
+    public Map<String, String>getParams(){
+
+
+        return parameters;
+    }
+}
